6e174d6b681eaff1cee07e409a4ae0034c24f1c8
Igor Sousa <arqurb.igor@gmail.com>
2025-07-23 13:36:05 -0300

    Add project files.

---
+++ src/AgendaContatos.Api/Controllers/ContactsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaContatos.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateContact([FromBody] string contactName)
+        {
+            // Here you would typically add logic to save the contact to a database
+            // For this example, we will just return a success message
+            return Created();
+        }
+    }
+}
