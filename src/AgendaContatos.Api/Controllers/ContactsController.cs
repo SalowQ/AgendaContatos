b56524bb47830722acffcabd0a4097db4891be50
Igor Sousa <arqurb.igor@gmail.com>
2025-07-23 17:42:22 -0300

    feat: implementar filtro de exceptions

--- src/AgendaContatos.Api/Controllers/ContactsController.cs
+++ src/AgendaContatos.Api/Controllers/ContactsController.cs
@@ -13,23 +13,9 @@ namespace AgendaContatos.Api.Controllers
         [HttpPost]
         public IActionResult Create([FromBody] RequestCreateContactJson request)
         {
-            try
-            {
                 var useCase = new CreateContactUseCase();
                 var response = useCase.Execute(request);
                 return Created(string.Empty, response);
-            }
-            catch (ErrorOnValidationException ex)
-            {
-                var errorResponse = new ResponseErrorJson(ex.Errors);
-                return BadRequest(errorResponse);
-            }
-            catch
-            {
-                var errorResponse = new ResponseErrorJson("Erro inesperado.");
-                return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
-            }
-
         }
     }
 }
