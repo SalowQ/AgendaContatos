d76601c8d133f721ac956dbc9fbaecdd8451d4aa
Igor Sousa <arqurb.igor@gmail.com>
2025-07-24 11:39:53 -0300

    feat: criar teste unitário para criação de contato correto

---
+++ tests/CommonTestUtilities/Requests/RequestCreateContactJsonBuilder.cs
@@ -0,0 +1,16 @@
+﻿using AgendaContatos.Communication.Requests;
+using Bogus;
+
+namespace CommonTestUtilities.Requests
+{
+    public class RequestCreateContactJsonBuilder
+    {
+        public static RequestCreateContactJson Build()
+        {
+            return new Faker<RequestCreateContactJson>("pt_BR")
+                .RuleFor(c => c.ContactName, f => f.Person.FullName)
+                .RuleFor(c => c.ContactEmail, f => f.Internet.Email())
+                .RuleFor(c => c.ContactPhone, f => f.Phone.PhoneNumber("###########"));
+        }
+    }
+}
---
+++ tests/Validators.Tests/Contacts/Create/CreateContactValidatorTests.cs
@@ -0,0 +1,21 @@
+﻿using AgendaContatos.Application.UseCases.Contacts.Create;
+using CommonTestUtilities.Requests;
+using Shouldly;
+
+namespace Validators.Tests.Contacts.Create
+{
+    public class CreateContactValidatorTests
+    {
+        [Fact]
+        public void Sucess()
+        {
+            //Arrange
+            var validator = new CreateContactValidator();
+            var request = RequestCreateContactJsonBuilder.Build();
+            //Act
+            var result = validator.Validate(request);
+            //Assert
+            result.IsValid.ShouldBe(true);
+        }
+    }
+}
