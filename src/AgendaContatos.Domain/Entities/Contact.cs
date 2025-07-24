c31622204c5eaf8d24ebc6ff2e8ffe9ec97aa90a
Igor Sousa <arqurb.igor@gmail.com>
2025-07-24 17:42:50 -0300

    feat: criar entitade de contato

---
+++ src/AgendaContatos.Domain/Entities/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace AgendaContatos.Domain.Entities
+{
+    public class Contact
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+    }
+}
