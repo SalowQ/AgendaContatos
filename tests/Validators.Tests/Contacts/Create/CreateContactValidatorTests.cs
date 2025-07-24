d10455fdcf15b5c2d90985a6603f0ed805da2ee0
Igor Sousa <arqurb.igor@gmail.com>
2025-07-24 14:51:59 -0300

    feat: adicionar teste de erro no cadastro de contato

--- tests/Validators.Tests/Contacts/Create/CreateContactValidatorTests.cs
+++ tests/Validators.Tests/Contacts/Create/CreateContactValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using AgendaContatos.Application.UseCases.Contacts.Create;
+using AgendaContatos.Exception;
 using CommonTestUtilities.Requests;
 using Shouldly;
 
@@ -16,6 +17,24 @@ namespace Validators.Tests.Contacts.Create
             var result = validator.Validate(request);
             //Assert
             result.IsValid.ShouldBe(true);
+        }
+
+        [Fact]
+        public void Error_Name_Empty()
+        {
+            //Arrange
+            var validator = new CreateContactValidator();
+            var request = RequestCreateContactJsonBuilder.Build();
+            request.ContactName = string.Empty;
+            //Act
+            var result = validator.Validate(request);
+            //Assert
+            result.IsValid.ShouldBe(false);
+            result.Errors.ShouldSatisfyAllConditions(
+                () => result.Errors.Count.ShouldBe(2),
+                () => result.Errors[0].ErrorMessage.ShouldBe(ResourceErrorMessages.NAME_REQUIRED),
+                () => result.Errors[1].ErrorMessage.ShouldBe(ResourceErrorMessages.NAME_MINIMUM)
+            );
         }
     }
 }
