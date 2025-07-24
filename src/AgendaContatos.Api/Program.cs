9ac9752202ebd3fe9f87f64a6a5bbe184b3a4ec8
Igor Sousa <arqurb.igor@gmail.com>
2025-07-24 10:21:45 -0300

    feat: criar middleware de linguagem

--- src/AgendaContatos.Api/Program.cs
+++ src/AgendaContatos.Api/Program.cs
@@ -1,4 +1,5 @@
 using AgendaContatos.Api.Filters;
+using AgendaContatos.Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +19,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<CultureMiddleware>();
 
 app.UseHttpsRedirection();
 
