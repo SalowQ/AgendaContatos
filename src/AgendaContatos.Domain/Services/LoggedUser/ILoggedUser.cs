5ee15e2ba341792623667e07084f059324fb5f81
Igor Sousa <arqurb.igor@gmail.com>
2025-07-29 00:06:48 -0300

    feat: iniciar leitura de token

---
+++ src/AgendaContatos.Domain/Services/LoggedUser/ILoggedUser.cs
@@ -0,0 +1,9 @@
+﻿using AgendaContatos.Domain.Entities;
+
+namespace AgendaContatos.Domain.Services.LoggedUser
+{
+    public interface ILoggedUser
+    {
+        Task<User> Get();
+    }
+}
---
+++ src/AgendaContatos.Infrastructure/Services/LoggedUser.cs
@@ -0,0 +1,26 @@
+﻿using AgendaContatos.Domain.Entities;
+using AgendaContatos.Domain.Services.LoggedUser;
+using AgendaContatos.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace AgendaContatos.Infrastructure.Services
+{
+    internal class LoggedUser : ILoggedUser
+    {
+        private readonly AgendaContatosDbContext _dbContext;
+        public LoggedUser(AgendaContatosDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<User> Get()
+        {
+            string token = "token";
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var jwtSecurityToken = tokenHandler.ReadJwtToken(token);
+            var identifier = jwtSecurityToken.Claims.First(claim => claim.Type == ClaimTypes.Sid).Value;
+            return await _dbContext.Users.AsNoTracking().FirstAsync(user => user.UserIdentifier == Guid.Parse(identifier));
+        }
+    }
+}
